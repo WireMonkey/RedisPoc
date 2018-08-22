85861488e89aad8da3abba55444bba9a7a04c5dd
Josh Kofoot <JoshK@spotcable.com>
2018-08-22 16:35:08 -0500

    Added basic redis logic

---
+++ RedisPoc.Business/Class1.cs
@@ -0,0 +1,37 @@
+﻿using ServiceStack.Redis;
+using System;
+using System.Collections.Generic;
+
+namespace RedisPoc.Business
+{
+    public class RedisLogic
+    {
+        private RedisManagerPool manager { get; set; }
+
+        public RedisLogic()
+        {
+            manager = new RedisManagerPool("");
+        }
+
+        public T GetData<T>(string key)
+        {
+            using (var client = manager.GetClient())
+            {
+                var data = client.Get<T>(key);
+
+                return data;
+            }
+        }
+
+        public bool SetListData(string key,List<string> data)
+        {
+            var x = new List<string>();
+            using (var client = manager.GetClient())
+            {
+                client.AddRangeToList(key, data);
+
+                return true;
+            }
+        }
+    }
+}
