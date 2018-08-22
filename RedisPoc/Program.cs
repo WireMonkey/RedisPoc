7fa07771a4cda72bf666bd0c98ee1fb3dd2ec581
Josh Kofoot <JoshK@spotcable.com>
2018-08-22 14:01:14 -0500

    Inital Comit

---
+++ RedisPoc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RedisPoc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
